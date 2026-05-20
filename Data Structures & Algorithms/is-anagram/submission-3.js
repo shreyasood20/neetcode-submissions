c80c2f3ded498de4c3e7036facf14b9c6af0e890
shreyasood20 <101243284+shreyasood20@users.noreply.github.com>
2026-05-20 13:07:55 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length){
+            return false
+        }
+        const freqS = {};
+        const freqT={}
+        for(let i =0;i<s.length;i++){
+            freqS[s[i]]= (freqS[s[i]]||0)+1
+            freqT[t[i]]=(freqT[t[i]]||0)+1
+        }
+        //if(Object.keys(freqS).length!==Object.keys(freqT).length)return false;
+        for(let char of s){
+            if(freqS[char] !=freqT[char]) return false
+        }
+        return true
+        
+    }
+}
