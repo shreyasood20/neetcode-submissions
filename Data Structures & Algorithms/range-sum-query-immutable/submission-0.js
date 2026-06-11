bc9442c998175140666abb8e035fcaf9d1de8366
shreyasood20 <101243284+shreyasood20@users.noreply.github.com>
2026-06-11 09:48:38 +0530

    Add: range-sum-query-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-0.js
@@ -0,0 +1,24 @@
+class NumArray {
+    /**
+     * @param {number[]} nums
+     */
+    constructor(nums) {
+        const n = nums.length;
+        this.prefix = Array(n);
+        this.prefix[0] = nums[0];
+        for (let i = 1; i < n; i++) {
+            this.prefix[i] = this.prefix[i - 1] + nums[i];
+        }
+    }
+
+    /**
+     * @param {number} left
+     * @param {number} right
+     * @return {number}
+     */
+    sumRange(left, right) {
+        const leftValue = left===0?0:this.prefix[left - 1]
+        const sum = this.prefix[right] - leftValue;
+        return sum;
+    }
+}
