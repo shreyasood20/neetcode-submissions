f075933354fb2d11cf095f5a8d735708160bdc56
shreyasood20 <101243284+shreyasood20@users.noreply.github.com>
2026-07-21 13:43:36 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10  
+print(type(variable))
+variable = 0.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'string'
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
