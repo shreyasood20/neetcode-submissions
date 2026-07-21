85883970fa433e7725bad60f30b88ec05a275fbf
shreyasood20 <101243284+shreyasood20@users.noreply.github.com>
2026-07-21 17:29:27 +0530

    Add: python-string-formatting - submission-0

---
+++ Python For Beginners/python-string-formatting/submission-0.py
@@ -0,0 +1,8 @@
+def say_goodbye(name: str, hour: int) -> str:
+    return "Goodbye, {0}. See you again at {1} o'clock.".format(name,hour)
+
+
+# do not modify below this line
+print(say_goodbye("Bob", 12))
+print(say_goodbye("Jane", 4))
+print(say_goodbye("NeetCode", 9))
